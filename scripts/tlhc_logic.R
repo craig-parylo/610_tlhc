b5bf8b70f3e4bcbfe73b001a65f7d20013519950
Craig Parylo <craig.parylo2@nhs.net>
2023-01-18 10:38:30 +0000

    updates path to scripts

--- scripts/tlhc_logic.R
+++ scripts/tlhc_logic.R
@@ -9,28 +9,28 @@ library(here)
 # Preparation ------------------------------------------------------------------
 
 # Download tables from SQL server (estimated time = 17 mins)
-source(here('scripts', 'tlhc_scripts', 'tlhc_download_sql_data.R'))
+source(here('scripts', 'tlhc_download_sql_data.R'))
 rm(list=ls())
 
 # Process the downloaded files (estimated time = 30 seconds)
-source(here('scripts', 'tlhc_scripts', 'tlhc_process_sql_data.R'))
+source(here('scripts', 'tlhc_process_sql_data.R'))
 rm(list=ls())
 
 # Calculate metrics (estimated time = 2 mins)
-source(here('scripts', 'tlhc_scripts', 'tlhc_metrics.R'))
+source(here('scripts', 'tlhc_metrics.R'))
 rm(list=ls())
 
 
 # DQ / ad-hoc analysis ---------------------------------------------------------
-file.edit(here('scripts', 'tlhc_scripts', 'tlhc_ad_hoc_analysis.R'))
+file.edit(here('scripts', 'tlhc_ad_hoc_analysis.R'))
 
-file.edit(here('scripts', 'tlhc_scripts', 'tlhc_validation.R'))
+file.edit(here('scripts', 'tlhc_validation.R'))
 
 
 # Reporting --------------------------------------------------------------------
 
 # output datasheets
-source(here('scripts', 'tlhc_scripts', 'tlhc_datasheet_output.R'))
+source(here('scripts', 'tlhc_datasheet_output.R'))
 rm(list=ls())
 
 # transfer datasheets
@@ -41,7 +41,7 @@ rm(list=ls())
 
 
 # Produce demographic report (needs copying to the template)
-source(here('scripts', 'tlhc_scripts', 'tlhc_demographics.R'))
+source(here('scripts', 'tlhc_demographics.R'))
 rm(list=ls())
 
 # Produce flat file output
