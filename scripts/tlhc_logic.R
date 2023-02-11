71b85594d407593fb1a8e0dbc5a6c60e5aef6e6a
Craig Parylo <craig.parylo2@nhs.net>
2023-02-11 15:32:09 +0000

    updates the list of tasks

--- scripts/tlhc_logic.R
+++ scripts/tlhc_logic.R
@@ -8,7 +8,7 @@ library(here)
 
 # Preparation ------------------------------------------------------------------
 
-# Download tables from SQL server (estimated time = 17 mins)
+# Download tables from SQL server (estimated time = 30 mins)
 source(here('scripts', 'tlhc_download_sql_data.R'))
 rm(list=ls())
 
@@ -29,19 +29,26 @@ file.edit(here('scripts', 'tlhc_validation.R'))
 
 # Reporting --------------------------------------------------------------------
 
-# output datasheets
+# output datasheets (estimated time = 1 mins)
+file.edit(here('scripts', 'tlhc_datasheet_output.R')) # to edit how many months back to produce the datasheets
 source(here('scripts', 'tlhc_datasheet_output.R'))
 rm(list=ls())
 
-# transfer datasheets
-
-# collate from DATA NEW
-
-# Produce TLHC report
-
-
-# Produce demographic report (needs copying to the template)
-source(here('scripts', 'tlhc_demographics.R'))
+# transfer datasheets (estimated time = 2 mins)
+source(here('scripts', 'tlhc_datasheet_transfer.R'))
 rm(list=ls())
 
-# Produce flat file output
+# collate from DATA NEW (estimated time = 5 mins)
+file.edit(here('scripts', 'tlhc_collate_datasheets.R')) # in case you need to edit the end date (when doing an out-of-sync report run)
+source(here('scripts', 'tlhc_collate_datasheets.R'))
+rm(list=ls())
+
+# Produce TLHC report (estimated time = 2 mins)
+file.edit(here('scripts', 'tlhc_report.R'))
+source(here('scripts', 'tlhc_report.R'))
+rm(list=ls())
+
+# Produce demographic report (estimated time = 1 minute)
+# NB, output needs copying to the demographics report template
+source(here('scripts', 'tlhc_demographics.R'))
+rm(list=ls())
