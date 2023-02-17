9346c29d52db017cdb53d571c8be04b1ffd677ae
Craig Parylo <craig.parylo2@nhs.net>
2023-02-17 07:30:35 +0000

    adds row counts per month

--- scripts/sql_check_monthly_activity_for_transaction.sql
+++ scripts/sql_check_monthly_activity_for_transaction.sql
@@ -6,40 +6,48 @@ Use these scripts to identify whether a given transaction id contains activity i
 
 -- Set a variable to hold transaction IDs
 DECLARE @TransID INT; 
-SET @TransID = 189740;
+SET @TransID = 188743;
 
 -- Invites
 SELECT DISTINCT
 	'Invites' AS [Table],
-	CONVERT(varchar(6), CONVERT(date, [First_Letter_Date], 104), 112) AS [calc_YearMon]
+	CONVERT(varchar(6), CONVERT(date, [First_Letter_Date], 104), 112) AS [calc_YearMon],
+	COUNT(ParticipantID) AS [Rows]
 FROM [TLHC_Reporting].[dbo].[tbTLHCTLHC_Pathway_Invite]
 WHERE TransactionId = @TransID
+GROUP BY CONVERT(varchar(6), CONVERT(date, [First_Letter_Date], 104), 112)
 ORDER BY [calc_YearMon] desc
 ;
 
 -- LHC
 SELECT DISTINCT
 	'LHC' AS [Table],
-	CONVERT(varchar(6), CONVERT(date, [LHC_Date], 104), 112) AS [calc_YearMon]
+	CONVERT(varchar(6), CONVERT(date, [LHC_Date], 104), 112) AS [calc_YearMon],
+	COUNT(ParticipantID) AS [Rows]
 FROM [TLHC_Reporting].[dbo].[tbTLHCTLHC_LungHealthCheck]
 WHERE TransactionId = @TransID
+GROUP BY CONVERT(varchar(6), CONVERT(date, [LHC_Date], 104), 112)
 ORDER BY [calc_YearMon] desc
 ;
 
 -- LDCT
 SELECT DISTINCT
 	'LDCT' AS [Table],
-	CONVERT(varchar(6), CONVERT(date, [LDCT_Date], 104), 112) AS [calc_YearMon]
+	CONVERT(varchar(6), CONVERT(date, [LDCT_Date], 104), 112) AS [calc_YearMon],
+	COUNT(ParticipantID) AS [Rows]
 FROM [TLHC_Reporting].[dbo].[tbTLHCTLHC_Pathway_LDCT]
 WHERE TransactionId = @TransID
+GROUP BY CONVERT(varchar(6), CONVERT(date, [LDCT_Date], 104), 112)
 ORDER BY [calc_YearMon] desc
 ;
 
 -- Smoking
 SELECT DISTINCT
 	'Smoking' AS [Table],
-	CONVERT(varchar(6), CONVERT(date, [Date_Offered_Smoking_Cessation], 104), 112) AS [calc_YearMon]
+	CONVERT(varchar(6), CONVERT(date, [Date_Offered_Smoking_Cessation], 104), 112) AS [calc_YearMon],
+	COUNT(ParticipantID) AS [Rows]
 FROM [TLHC_Reporting].[dbo].[tbTLHCTLHC_SmokingCessation]
 WHERE TransactionId = @TransID
+GROUP BY CONVERT(varchar(6), CONVERT(date, [Date_Offered_Smoking_Cessation], 104), 112)
 ORDER BY [calc_YearMon] desc
 ;
