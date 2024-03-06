2c1a71d24cfd2a0461842ca75c4ba331124554b4
Craig Parylo <craig.parylo2@nhs.net>
2024-03-06 08:48:52 +0000

    adds functions to load and process cancer diagnosis data

--- scripts/tlhc_metric_functions.R
+++ scripts/tlhc_metric_functions.R
@@ -2398,6 +2398,311 @@ get_df_metric_13_median_inv_to_diag <- function() {
   )
 }
 
+## based on cancer data --------------------------------------------------------
+
+#' Get a cancer group lookup (icd10 -> cancer group)
+#' 
+#' load cancer grouping map table (maps icd-10 codes to broad anatomical site)
+#' based on https://digital.nhs.uk/ndrs/data/data-sets/cosd/cosd-user-guide-v10/appendix
+#' 
+#' @return Tibble
+get_df_icd_group <- function() {
+
+  return(
+    vroom::vroom(
+      file = here('data', 'secret', 'tumour_groups.csv'),
+      show_col_types = F # don't include any console output when reading file
+    ) |>
+      mutate(
+        # remove the dot from icd
+        icd_10 = str_remove_all(icd_10, '\\.'),
+        
+        # make a 3-character icd reference
+        icd10_3c = str_sub(icd_10, 1, 3),
+        
+        # Convert 'other' and codes with multiple groups to 'Unknown' in keeping with TUMOUR_GROUP
+        tumour_site_group_3c = case_when(
+          icd10_3c %in% c('C76', 'C77', 'C78', 'C79', 'C80', 'C74', 'C75', 'C97') ~ 'Unknown',
+          .default = tumour_site_group
+        )
+      ) |>
+      # Add lookups for 'D' codes - which don't appear in this list but are found in rr_tumour TUMOUR_GROUP
+      add_row(icd10_3c = 'D09', tumour_site_group_3c = 'Urological') |>
+      add_row(icd10_3c = 'D41', tumour_site_group_3c = 'Urological') |>
+      add_row(icd10_3c = 'D32', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D33', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D35', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D42', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D43', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D44', tumour_site_group_3c = 'Brain/Central Nervous System') |>
+      add_row(icd10_3c = 'D06', tumour_site_group_3c = 'Gynaecological') |>
+      add_row(icd10_3c = 'D29', tumour_site_group_3c = 'Urological') |>
+      add_row(icd10_3c = 'D48', tumour_site_group_3c = 'Unknown')
+  )
+}
+
+#' Group diagnoses
+#' 
+#' Use the icd-10 to cancer group lookup to group cancers
+#' 
+#' @param df Tibble of tumour data (either cancer registry or rr tumour) to group cancers
+#' @param df_icd_group Tibble of lookup values from icd10 code to tumour group
+#' @param type Character description of the tibble passed in df - either 'registry' or 'tumour'
+#' @return Tibble
+group_cancers <- function(df, df_icd_group, type = 'registry') {
+  
+  # get a lookup between icd10 and cancer group
+  icd_group_lu <- df_icd_group |> 
+    filter(!is.na(icd_10)) |> 
+    select(icd_10, tumour_site_group) |> 
+    unique()
+  
+  # get a 3-char code lookup for icd10 to cancer group
+  icd_group_lu_3c <- df_icd_group |> 
+    select(icd10_3c, tumour_site_group_3c) |> 
+    unique()
+  
+  # decide if 'registry' or 'tumour' table
+  if (type == 'registry') {
+    
+    # this is the registry table - requires lookups from full-code and 3-code icd
+    return(
+      df |>
+        # add in codes full matched
+        left_join(
+          y = icd_group_lu |>
+            select(
+              SITE_ICD10R4_O2_FROM2013 = icd_10,
+              tumour_site_group_1 = tumour_site_group
+            ) |>
+            unique(),
+          by = 'SITE_ICD10R4_O2_FROM2013'
+        ) |>
+        # add in 3-char codes lookup
+        left_join(
+          y = icd_group_lu_3c |>
+            select(
+              SITE_ICD10R4_O2_3CHAR_FROM2013 = icd10_3c,
+              tumour_site_group_2 = tumour_site_group_3c
+            ) |>
+            unique(),
+          by = 'SITE_ICD10R4_O2_3CHAR_FROM2013'
+        ) |>
+        # coalesce these to get the best response
+        mutate(tumour_site_group = coalesce(tumour_site_group_1, tumour_site_group_2)) |>
+        select(-tumour_site_group_1, -tumour_site_group_2)
+    )
+    
+  } else {
+    
+    # this is a tumour table - only requires 3-code icd
+    return(
+      df |>
+        # add in codes full matched
+        left_join(
+          y = icd_group_lu_3c |>
+            select(TUMOUR_SITE = icd10_3c, tumour_site_group = tumour_site_group_3c) |>
+            unique(),
+          by = 'TUMOUR_SITE'
+        )
+    )
+    
+  }
+}
+
+#' Identify tumours in cancer registry
+#' 
+#' Looks up the corresponding cancer registry TUMOUR_ID (where applicable)
+#' 
+#' @param df Tibble of RR tumours
+#' @param df_events Tibble as a subset of RR pathway containing just dates of diagnoses
+#' @return Tibble
+identify_registry_tumourid <- function(df, df_events) {
+  
+  # step 1 link RR tumours with cancer registry tumours
+  df_lookup <- df |> 
+    # small subset of data
+    select(TUMOUR_AVP_ID, ParticipantID, DIAGNOSISDATE) |>
+    unique() |> 
+    # add in lookup values
+    left_join(
+      y = df_events,
+      by = 'ParticipantID',
+      relationship = 'many-to-many' # there will be multiple diagnosis records per rr tumour
+    ) |> 
+    # work out difference between dates
+    mutate(
+      days_difference = date_count_between(
+        start = DIAGNOSISDATE |> as.Date(),
+        end = EVENT_DATE |> as.Date(),
+        precision = 'day'
+      ) |> 
+        abs()
+    ) |> 
+    # remove large outliers
+    filter(days_difference <= 40) |> 
+    # keep best match per tumour
+    group_by(TUMOUR_AVP_ID) |> 
+    slice_min(n = 1, order_by = days_difference) |> 
+    ungroup() |>
+    # simplify output
+    select(TUMOUR_AVP_ID, TUMOUR_ID) |> 
+    unique()
+    
+  # step 2 - return the rr tumour table with identified TUMOUR_IDs
+  return(
+    df |> 
+      left_join(
+        y = df_lookup,
+        by = 'TUMOUR_AVP_ID'
+      )
+  )
+
+}
+
+
+#' Get a dataframe of consolidated cancer records
+#' 
+#' @return Tibble
+get_df_cancer <- function() {
+  
+  p('df cancer outcomes')
+  
+  # load the data if not already loaded
+  if(!exists('df_canc_registry')){df_canc_registry <- load_df_cancer_reg()}
+  if(!exists('df_canc_rr_tumour')){df_canc_rr_tumour <- load_df_cancer_tumour()}
+  if(!exists('df_icd_group')){df_icd_group <- get_df_icd_group()}
+  
+  ## get a list of diagnosis events ---
+  df_events <- df_canc_rr_pathway |> 
+    # subset fields for efficiency
+    select(ParticipantID, EVENT_DATE, EVENT_TYPE, TUMOUR_ID) |> 
+    # limit to diagnosis type events
+    filter(EVENT_TYPE %in% c(6, 100)) |>
+    # remove any duplication
+    unique()
+  
+  ## tumour staging group ---
+  df_canc_registry <- df_canc_registry |> 
+    # add some groupings
+    group_cancers(
+      df_icd_group = df_icd_group,
+      type = 'registry'
+    ) |> 
+    mutate(stage_group = str_sub(STAGE_BEST, 1, 1))
+  
+  df_canc_rr_tumour <- df_canc_rr_tumour |> 
+    # add some groupings
+    group_cancers(
+      df_icd_group = df_icd_group,
+      type = 'tumour'
+    ) |> 
+    mutate(stage_group = str_sub(STAGE, 1, 1)) |> 
+    # identify tumours present in cancer registry
+    identify_registry_tumourid(df_events = df_events)
+  
+  
+  # return a consolidated dataset
+  return(
+    bind_rows(
+      df_canc_registry |> 
+        select(
+          # keys
+          pk = TUMOUR_ID,      # native key
+          ParticipantID,       # TLHC key
+          
+          # tumour
+          diagnosis_date = DIAGNOSISDATEBEST,
+          diagnosis_icd10_3c = SITE_ICD10R4_O2_3CHAR_FROM2013,
+          stage = STAGE_BEST,
+          tumour_site_group = tumour_site_group,
+          stage_group = stage_group,
+          
+          # patient demographics
+          gender_code = GENDER,
+          ethnicity_code = ETHNICITY,
+          age = AGE,
+          imd_quintile = IMD_QUINTILE,
+          
+          # geography
+          trust_code = DIAG_TRUST,
+          cancer_alliance = CANALLIANCE_2021_NAME
+        ) |> 
+        mutate(source = 'NCRAS Cancer Registry'),
+      
+      df_canc_rr_tumour |>
+        filter(
+          !TUMOUR_ID %in% df_canc_registry$TUMOUR_ID, # exclude records covered in cancer registry
+          DIAGNOSISDATE |> as.Date() > max(df_canc_registry$DIAGNOSISDATEBEST) # only include diagnoses after registry ends
+        ) |> 
+        select(
+          # keys
+          pk = TUMOUR_AVP_ID,
+          ParticipantID,
+          
+          # tumour
+          diagnosis_date = DIAGNOSISDATE,
+          diagnosis_icd10_3c = TUMOUR_SITE,
+          stage = STAGE,
+          tumour_site_group = tumour_site_group,
+          stage_group = stage_group,
+          
+          # patient demographics
+          gender_code = GENDER,
+          ethnicity_code = ETHNICCATEGORY,
+          age = PatientAge,
+          imd_quintile = QUINTILE_2019,
+          
+          # geography
+          trust_code = TRUST_CODE,
+          cancer_alliance = CANALLIANCE_2021_NAME
+        ) |> 
+        mutate(source = 'NCRAS RR Tumour'),
+    ) |> 
+      # look up coded values and convenience fields
+      mutate(
+        diagnosis_date_ym = diagnosis_date |> as.yearmon(),
+        gender = case_match(
+          gender_code,
+          0 ~ 'Not known',
+          1 ~ 'Male',
+          2 ~ 'Female',
+          9 ~ 'Not specified'
+        ),
+        ethnicity = case_match(
+          ethnicity_code,
+          'A' ~ 'British', 
+          'B' ~ 'Irish', 
+          'C' ~ 'Any other White background', 
+          'D' ~ 'White and Black Caribbean', 
+          'E' ~ 'White and Black African', 
+          'F' ~ 'White and Asian', 
+          'G' ~ 'Any other mixed background', 
+          'H' ~ 'Indian', 
+          'J' ~ 'Pakistani', 
+          'K' ~ 'Bangladeshi', 
+          'L' ~ 'Any other Asian background', 
+          'M' ~ 'Caribbean', 
+          'N' ~ 'African', 
+          'P' ~ 'Any other Black background', 
+          'R' ~ 'Chinese', 
+          'S' ~ 'Any other ethnic group', 
+          'Z' ~ 'Not stated', 
+          'X' ~ 'Not Known'
+        ),
+        ethnicity_group = case_match(
+          ethnicity_code,
+          c('0', 'A', 'B', 'C') ~ 'White',
+          c('D', 'E', 'F', 'G') ~ 'Mixed',
+          c('H', 'J', 'K', 'L') ~ 'Asian',
+          c('M', 'N', 'P') ~ 'Black',
+          c('R', 'S') ~ 'Other',
+          c('Z') ~ 'Not stated',
+          c(99, '99', 'X') ~ 'Not known'
+        )
+      )
+  )
+}
 
 ## calculate aggregate metrics -------------------------------------------------
 
